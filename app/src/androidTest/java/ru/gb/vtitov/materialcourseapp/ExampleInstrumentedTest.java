f62e4b5ab0adbe901019544ebb0b66a5710b1246
Valentin Titov <iosdevelop@yandex.ru>
2019-03-16 12:12:18 +0300

    Impletemted bottom navigation bar
    Impletemted bottom navigation bar with 3 fragments and increment badge view by  timer

--- app/src/androidTest/java/ru/gb/vtitov/materialcourseapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/gb/vtitov/materialcourseapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
-package ru.gb.vtitov.materialcourse;
+package ru.gb.vtitov.materialcourseapp;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -16,11 +17,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+	@Test
+	public void useAppContext() {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ru.gb.vtitov.materialcourse", appContext.getPackageName());
-    }
+		assertEquals("ru.gb.vtitov.materialcourseapp", appContext.getPackageName());
+	}
 }
